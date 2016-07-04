4675c8cdaf4532eee8a0dbedcb317971031ccd14
DimasSup <admin@iMac.local>
2016-07-04 15:37:02 +0300

    add macros

--- HPManagedObjects/Classes/HelpClasses/Macroses.h
+++ HPManagedObjects/Classes/HelpClasses/Macroses.h
@@ -11,6 +11,8 @@
 
 #define THOROW_NOT_IMPLEMENT @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%@: Should implement in extension:[%@ %@]",[NSBundle mainBundle].bundleIdentifier,[[self class]className], NSStringFromSelector(_cmd)] userInfo:nil];
 
+#define THOROW_NOT_IMPLEMENT_MESSAGE(message) @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"%@: %@\n Should implement in extension:[%@ %@]",[NSBundle mainBundle].bundleIdentifier,message,[[self class]className], NSStringFromSelector(_cmd)] userInfo:nil];
+
 #define NSStringFormat(format, ...) [NSString stringWithFormat:format,## __VA_ARGS__]
 
 #endif /* Macroses_h */
