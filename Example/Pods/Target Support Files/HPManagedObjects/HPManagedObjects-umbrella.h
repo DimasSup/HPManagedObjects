729621a98e7d2d87a143459f02ff83422d2cd17e
Dimas <dima.teleban@gmail.com>
2016-12-31 00:12:46 +0200

    no message

--- Example/Pods/Target Support Files/HPManagedObjects/HPManagedObjects-umbrella.h
+++ Example/Pods/Target Support Files/HPManagedObjects/HPManagedObjects-umbrella.h
@@ -7,13 +7,13 @@
 #import "Macroses.h"
 #import "NSObject+CallInstanceMethod.h"
 #import "NSObject+ClassName.h"
-#import "BaseManagedObjectModel+FMDB.h"
 #import "BaseManagedObjectModel.h"
 #import "BaseManagedObjectModel_Private.h"
 #import "Mapping.h"
 #import "MappingDescriptor.h"
-#import "TypeSelector.h"
 #import "SerializeHelper.h"
+#import "TypeSelector.h"
+#import "BaseManagedObjectModel+FMDB.h"
 
 FOUNDATION_EXPORT double HPManagedObjectsVersionNumber;
 FOUNDATION_EXPORT const unsigned char HPManagedObjectsVersionString[];
