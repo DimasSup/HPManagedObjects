e65d59cf81f0e6f40bb61ed02d11b1baa7443254
DimasSup <admin@iMac.local>
2018-01-12 18:59:56 +0200

    no message

--- HPManagedObjects/Classes/HelpClasses/Generic.m
+++ HPManagedObjects/Classes/HelpClasses/Generic.m
@@ -48,6 +48,7 @@ static int GenericHelper_isRetina = -1;
 		CFUUIDRef theUUID = CFUUIDCreate(NULL);
 		CFStringRef string = CFUUIDCreateString(NULL, theUUID);
 		CFRelease(theUUID);
+		CFAutorelease(string);
 		return (__bridge NSString *)string;
 	}
 	
